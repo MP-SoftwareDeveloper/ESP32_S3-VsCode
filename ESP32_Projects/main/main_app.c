03390f0b2981cb151acafa76af2a9636a077f60a
MP-SoftwareDeveloper <morteza.pourmehdi.se@gmail.com>
2024-08-28 21:10:22 +0200

    RTOS toggle an LED GPIO13. 2024-08-28

--- ESP32_Projects/main/main_app.c
+++ ESP32_Projects/main/main_app.c
@@ -1,27 +1,32 @@
-
+// Link for source: https://www.youtube.com/watch?v=LHCZPkoHGNc
 #include <stdio.h>
-//#include "esp_mac.h"
-#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
- 
+#include "freertos/task.h" 
+#include "driver/gpio.h"
+
+//CONFIG_FREERTOS_UNICORE
+
+void blink(void)
+{
+    gpio_set_direction(GPIO_NUM_13,GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+        gpio_set_level(GPIO_NUM_13,0);
+        vTaskDelay(1500/portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_13,1 );
+        vTaskDelay(50/portTICK_PERIOD_MS);
+    }
+}
 void app_main(void)
 {
-  gpio_set_direction(GPIO_NUM_12,GPIO_MODE_DEF_INPUT);
-  gpio_set_pull_mode(GPIO_NUM_12,GPIO_PULLUP_ONLY);
-  gpio_set_direction(GPIO_NUM_13,GPIO_MODE_DEF_OUTPUT );
-
-  while (true)
-  {
-    //gpio_set_level(13,1);
-    if(gpio_get_level(GPIO_NUM_12))
-        gpio_set_level(GPIO_NUM_13,0);
-    else
-        gpio_set_level(GPIO_NUM_13,1);
-    /* code */
-
-    vTaskDelay(10);
-  }
-    
+    xTaskCreate(
+        blink,   // Function to be called
+     "LED blink",// Name of task
+      2048,      // Stack size(bytes in ESP32, words in freertos)
+     NULL ,      // Parameter to pass to function
+        1,       // Task Priority 0 lowest priority (configMAX_PRIORITIES -1)
+        0       // Handle_t to task
+      );
+  
 }
-
